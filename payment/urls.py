1d92a1d688ee3b374ebb7be96f4dd1e5b939ec72
babawale123 <waleramson124@gmail.com>
2024-06-10 13:01:46 +0100

    payment implementation

--- payment/urls.py
+++ payment/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PaystackWebhook
+from .views import PaystackWebhook,GetAllTransaction
 
 
 urlpatterns = [
+    path('', GetAllTransaction.as_view()),
     path('webhook/', PaystackWebhook.as_view(), name='paystack-webhook'),
 ]
